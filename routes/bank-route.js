c397171f789b1f1b5d553b9698cc599ab033ffab
sheriff ibrahim <ibrahimsheriff999@gmail.com>
2023-09-22 08:40:58 +0100

    added cors

--- routes/bank-route.js
+++ routes/bank-route.js
@@ -2,15 +2,24 @@ const express = require("express");
 const { Banks } = require("../model/bank");
 const { queryValidateBank } = require("../model/bank");
 const { User } = require("../model/user");
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
    try {
       const { error } = queryValidateBank(req.body);
-      if (error) res.status(400).send({ error: "Invalid body Parameter" });
+      if (error) res.status(400).send({ error: error.message });
       else {
          const isBankRecorded = await Banks.count({ phone: req.body.phone });
+         const user = await User.findOne({ phone: req.body.phone });
+         if (!user) return res.status(404).send({ error: "no user" });
+         const isPasswordVerified = await bcrypt.compare(
+            req.body.password,
+            user.password
+         );
+         if (!isPasswordVerified)
+            return res.status(400).send({ error: "bad password" });
          if (isBankRecorded === 1) {
             const isUpdated = await Banks.updateOne(
                { phone: req.body.phone },
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
 const app = express();
 
 const auth = require("./routes/auth");
@@ -16,6 +17,7 @@ mongoose
    })
    .catch((err) => console.log(err));
 app.use(express.json());
+app.use(cors())
 
 app.use("/auth", auth);
 app.use("/bank", bank);
